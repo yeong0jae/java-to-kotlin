5e03d13ac4db871568a59b807ffc3315fac005a6
Yeongjae Kim <sees77770@gmail.com>
2024-11-18 18:24:05 +0900

    boxing unboxing

---
+++ kotlin/src/main/kotlin/variable/BoxingUnboxing.kt
@@ -0,0 +1,10 @@
+package variable
+
+fun main() {
+    val t = System.currentTimeMillis()
+    var sum: Long = 0L
+    for (i in 0L..1_000_000L) {
+        sum += i
+    }
+    println("실행 시간: ${System.currentTimeMillis() - t} ms")
+}

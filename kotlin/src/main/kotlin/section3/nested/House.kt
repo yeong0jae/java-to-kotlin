d51001008dbab175338487c636ba9e2a660fda81
Yeongjae Kim <sees77770@gmail.com>
2024-11-24 23:37:59 +0900

    Inner Class

---
+++ kotlin/src/main/kotlin/section3/nested/House.kt
@@ -0,0 +1,15 @@
+package section3.nested
+
+class House(
+    private val address: String,
+    private val livingRoom: LivingRoom
+) {
+
+    inner class LivingRoom(
+        private val area: Double,
+    ) {
+        val address: String
+            get() = this@House.address
+    }
+
+}
